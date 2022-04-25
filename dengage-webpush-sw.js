e4de338c6d2086ca8f9cbe2f1e934626904861c3
sergen53 <57002662+sergen53@users.noreply.github.com>
2022-04-25 15:44:39 +0300

    Update dengage-webpush-sw.js
    new sdk

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -1,8 +1,7 @@
-
 var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 if (accountId && appGuid) {
-    importScripts("https://ptest.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://test-pub.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
         
